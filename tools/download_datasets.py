444dadc0390562b31a647b21d2a82ecde6d7d4b3
Jake VanderPlas <jakevdp@gmail.com>
2018-02-25 07:46:25 -0800

    ENH: refactor tooling and add additional datasets

--- tools/download_datasets.py
+++ tools/download_datasets.py
@@ -1,8 +1,16 @@
+"""
+Tool to download all local datasets and save them within the vega_datasets
+source tree.
+
+Usage:
+$ python download_datasets.py
+"""
+
 from os.path import abspath, join, dirname
 import sys
 import json
 
-sys.path.insert(1, abspath(join(dirname(__file__), '..', '..')))
+sys.path.insert(1, abspath(join(dirname(__file__), '..')))
 from vega_datasets.core import Dataset
 from vega_datasets._compat import urlretrieve
 
@@ -23,16 +31,16 @@ DATASETS_TO_DOWNLOAD = ['airports',
 def _download_datasets():
     """Utility to download datasets into package source"""
     def filepath(*args):
-        return abspath(join(dirname(__file__), *args))
+        return abspath(join(dirname(__file__), '..', 'vega_datasets', *args))
     dataset_listing = {}
     for name in DATASETS_TO_DOWNLOAD:
         data = Dataset(name)
         url = data.url
-        filename = filepath(data.filename)
+        filename = filepath('data', data.filename)
         print("retrieving data {0} -> {1}".format(url, filename))
         urlretrieve(url, filename)
         dataset_listing[name] = 'data/{0}'.format(data.filename)
-    with open(filepath('..', 'local_datasets.json'), 'w') as f:
+    with open(filepath('local_datasets.json'), 'w') as f:
         json.dump(dataset_listing, f, indent=2)
 
 
