c1442da9844c56a6db6a4071d6d37ba0bd10cb5d
Jake VanderPlas <jakevdp@gmail.com>
2018-02-25 11:02:03 -0800

    ENH: refactor tooling and add additional datasets

---
+++ tools/generate_datasets_json.py
@@ -0,0 +1,34 @@
+"""Tool to re-generate and save vega_datasets/datasets.json
+
+Usage:
+$ python generate_datasets_json.py /path/to/vega-datasets
+
+The provided path should be to the local clone of
+http://github.com/vega/vega-datasets/
+"""
+
+import os
+import sys
+import json
+
+
+def main(vega_datasets_package):
+    data_dir = os.path.join(vega_datasets_package, 'data')
+    datasets_file = os.path.join(os.path.dirname(__file__),
+                                 '..', 'vega_datasets',
+                                 'datasets.json')
+
+    datasets = {}
+    for filename in os.listdir(data_dir):
+        name, fmt = os.path.splitext(filename)
+        datasets[name] = {'filename': filename, 'format': fmt[1:]}
+
+    with open(datasets_file, 'w') as f:
+        json.dump(datasets, f, indent=2)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 2:
+        main(sys.argv[1])
+    else:
+        print(__doc__)
