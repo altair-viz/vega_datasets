5263c2d4740615578ccdc20f4dd9a27ed24e6c3d
Jake VanderPlas <jakevdp@gmail.com>
2018-05-15 09:56:44 -0700

    MAINT: clean up setup & distribution files

--- setup.py
+++ setup.py
@@ -2,11 +2,14 @@ import io
 import os
 import re
 
-from setuptools import setup
+from setuptools import setup, find_packages
+
+
+here = os.path.abspath(os.path.dirname(__file__))
 
 
 def read(path, encoding='utf-8'):
-    path = os.path.join(os.path.dirname(__file__), path)
+    path = os.path.join(here, path)
     with io.open(path, encoding=encoding) as fp:
         return fp.read()
 
@@ -23,59 +26,42 @@ def version(path):
     raise RuntimeError("Unable to find version string.")
 
 
-DESCRIPTION = "A Python package for offline access to Vega datasets"
-LONG_DESCRIPTION = """
-A Python package for offline access to Vega datasets available at
-https://github.com/vega/vega-datasets.
-
-This package has several goals:
-
-- Provide straightforward access to the datasets in vega-datasets
-- return the results in the form of a Pandas dataframe.
-- wherever dataset size and/or license constraints make it possible, bundle the dataset with the package so that datasets can be loaded in the absence of a web connection.
-
-Development at http://github.com/altair-viz/vega_datasets
-"""
-NAME = "vega_datasets"
-AUTHOR = "Jake VanderPlas"
-AUTHOR_EMAIL = "jakevdp@gmail.com"
-MAINTAINER = "Jake VanderPlas"
-MAINTAINER_EMAIL = "jakevdp@gmail.com"
-URL = 'http://github.com/altair-viz/vega_datasets'
-DOWNLOAD_URL = 'http://github.com/altair-viz/vega_datasets'
-LICENSE = 'MIT'
-
-VERSION = version('vega_datasets/__init__.py')
-
-setup(name=NAME,
-      version=VERSION,
-      description=DESCRIPTION,
-      long_description=LONG_DESCRIPTION,
-      author=AUTHOR,
-      author_email=AUTHOR_EMAIL,
-      maintainer=MAINTAINER,
-      maintainer_email=MAINTAINER_EMAIL,
-      url=URL,
-      download_url=DOWNLOAD_URL,
-      license=LICENSE,
+setup(name="vega_datasets",
+      version=version('vega_datasets/__init__.py'),
+      description="A Python package for offline access to Vega datasets",
+      long_description=read('README.md'),
+      long_description_content_type="text/markdown",
+      author="Jake VanderPlas",
+      author_email="jakevdp@gmail.com",
+      maintainer="Jake VanderPlas",
+      maintainer_email="jakevdp@gmail.com",
+      url='http://github.com/altair-viz/vega_datasets',
+      download_url='http://github.com/altair-viz/vega_datasets',
+      license="MIT",
       install_requires=["pandas"],
       tests_require=["pytest"],
-      packages=['vega_datasets',
-                'vega_datasets.tests',
-            ],
-      package_data={'vega_datasets': ['datasets.json',
-                                      'dataset_info.json',
-                                      'local_datasets.json',
-                                      os.path.join('_data', '*.json'),
-                                      os.path.join('_data', '*.csv'),
-                                      os.path.join('_data', '*.tsv')]},
+      packages=find_packages(exclude=['tools']),
+      package_data={
+          'vega_datasets': ['datasets.json',
+                            'dataset_info.json',
+                            'local_datasets.json',
+                            os.path.join('_data', '*.json'),
+                            os.path.join('_data', '*.csv'),
+                            os.path.join('_data', '*.tsv'),
+          ]
+      },
       classifiers=[
-        'Development Status :: 4 - Beta',
-        'Environment :: Console',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: BSD License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6'],
-     )
+          'Development Status :: 4 - Beta',
+          'Environment :: Console',
+          'Intended Audience :: Science/Research',
+          'License :: OSI Approved :: MIT License',
+          'Natural Language :: English',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+      ],
+      project_urls={
+          'Bug Reports': 'https://github.com/altair-viz/vega_datasets/issues',
+          'Source': 'https://github.com/altair-viz/vega_datasets',
+      },
+)
