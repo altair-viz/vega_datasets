1653a2955d89dfd5fd09c0f433cebcbd236312a2
Jake VanderPlas <jakevdp@google.com>
2019-12-03 01:09:41 -0800

    MAINT: update tools for syncing

--- vega_datasets/__init__.py
+++ vega_datasets/__init__.py
@@ -2,3 +2,7 @@ from vega_datasets.core import DataLoader, LocalDataLoader
 data = DataLoader()
 local_data = LocalDataLoader()
 __version__ = '0.8.0dev0'
+
+# This is the tag in http://github.com/vega/vega-datasets from
+# which the datasets in this repository are sourced.
+SOURCE_TAG = 'v1.24.0'
